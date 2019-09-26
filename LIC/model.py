6d47ac593405db080341c837253b86a2a3826046
arjuncm1997 <arjun>
2019-09-26 12:23:42 +0530

    re

---
+++ LIC/model.py
@@ -0,0 +1,58 @@
+from run import app
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class Customer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    adhar = db.Column(db.Integer)
+    dob = db.Column(db.Integer)
+    mobile = db.Column(db.Integer)
+    policy_name = db.Column(db.String(50))
+    amount = db.Column(db.Integer)
+    policydate = db.Column(db.DateTime, default=datetime.utcnow)
+    policyduration =db.Column(db.DateTime)
+    agencycode = db.Column(db.Integer,)
+    gender = db.Column(db.String(20))
+    address = db.Column(db.VARCHAR)
+
+   
+class Agent(db.Model):
+    agent_name = db.Column(db.String(50))
+    agent_id = db.Column(db.Integer, primary_key=True)
+    no_of_policy = db.Column(db.Integer)
+    email_id = db.Column(db.VARCHAR(50))
+    mobile = db.Column(db.Integer)
+    address = db.Column(db.VARCHAR)
+    qualification =db.Column(db.VARCHAR)
+    agency_date = db.Column(db.DateTime, default=datetime.utcnow)
+    group_name = db.Column(db.VARCHAR(120))
+    image = db.Column(db.LargeBinary)
+
+
+class Policy(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    policy_name = db.Column(db.VARCHAR)
+    policy_year = db.Column(db.Integer)
+
+
+class Meeting(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime)
+    time = db.Column(db.VARCHAR)
+
+
+class Competition(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    competition_date = db.Column(db.DateTime)
+    expiry_year = db.Column(db.DateTime)
+    no_of_policy = db.Column(db.Integer)
+
+
+class Group(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    group_nanme = db.Column(db.VARCHAR)    
---
+++ LIC/run.py
@@ -0,0 +1,145 @@
+from flask import Flask, render_template, request, redirect
+from model import *
+
+app =Flask(__name__)
+
+#//////////public////////////////
+
+@app.route('/pindex')
+def pindex():
+    return render_template("p_index.html")
+
+
+@app.route('/')
+def layout():
+    return render_template("p_layout.html")    
+
+#/////////////////////staff//////////////////
+
+
+@app.route('/cusform', methods=['POST', 'GET'])
+def cusform():
+    if request.method == 'POST':
+        customer_name = request.form['name']
+        customer_adhar = request.form['adhar']
+        customer_dob = request.form['dob']
+        customer_mobile = request.form['mobile']
+        customer_policyname = request.form['policyname']
+        customer_amount = request.form['amount']
+        customer_policydate = request.form['policydate']
+        customer_policyduration = request.form['policyduration']
+        customer_agencycode = request.form['agencycode']
+        customer_gender = request.form['gender']
+        customer_address = request.form['address']
+        new_customer = Customer(name=customer_name,adhar=customer_adhar,dob=customer_dob,mobile=customer_mobile,policy_name=customer_policyname,amount=customer_amount,policydate=customer_policydate,policyduration=customer_policyduration,agencycode=customer_agencycode,gender=customer_gender,address=customer_address)
+        try:
+            db.session.add(new_customer)
+            db.session.commit()
+            return redirect('/scusview')
+        except:
+            return 'There was an issue adding your task'  
+    else:
+        return render_template('s_addcustomer.html')
+
+
+@app.route('/smeetview')
+def smeetview():
+    return render_template("s_meetingview.html")    
+    
+
+@app.route('/details')
+def staff():
+    return render_template("s_layout.html")
+
+
+@app.route('/sindex')
+def sindex():
+    return render_template("s_index.html")
+
+
+@app.route('/meet')
+def schedule():
+    return render_template("s_meeting.html")
+
+
+@app.route('/policy')
+def policy():
+    return render_template("s_addpolicy.html")
+
+
+@app.route('/attend')
+def attendance():
+    return render_template("s_attendance.html")
+
+
+@app.route('/comp')
+def comp():
+    return render_template("s_competition.html")
+
+
+@app.route('/spolicyview')
+def spolicyview():
+    return render_template("s_policyview.html")
+
+
+@app.route('/scusview')
+def scusview():
+    customers = Customer.query.all()
+    return render_template("s_cusview.html", customer=customers)
+
+#//////////////////////agent////////////////////
+
+@app.route('/alay')
+def alay():
+    return render_template("a_layout.html")
+
+@app.route('/aindex')
+def aindex():
+    return render_template("a_index.html")  
+
+@app.route('/totalp')
+def ptotal():
+    return render_template("a_totalp.html")
+
+
+@app.route('/acusv')
+def acusv():
+    return render_template("a_customerview.html")  
+
+
+@app.route('/meetview')
+def ameetview():
+    return render_template("a_meetview.html")  
+
+
+#////////////////////////admin////////////////////////////////////
+
+@app.route('/admin')
+def admin():
+    return render_template("admin_layout.html")    
+
+@app.route('/admindex')
+def admindex():
+    return render_template("admin_index.html")    
+
+@app.route('/admagent')
+def admagent():
+    return render_template("admin_addagent.html")    
+     
+@app.route('/group')
+def group():
+    return render_template("admin_addgroup.html")     
+
+@app.route('/login')
+def login():
+    return render_template("login.html")    
+
+
+@app.route('/admaview')
+def agentview():
+    return render_template("admin_agentview.html")  
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)    
